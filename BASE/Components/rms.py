ab7d388e91236b39a7a1a43995b9952ffd068917
SaurabhKumbhar011 <saurabhkumbhar011@gmail.com>
2024-06-08 13:29:52 +0530

    First commit

---
+++ BASE/Components/rms.py
@@ -0,0 +1,9 @@
+import ctypes
+import mainwindow
+from mainwindow import MainWindow
+
+ctypes.windll.shcore.SetProcessDpiAwareness(1)
+
+if __name__ == "__main__":
+    app = mainwindow.MainWindow()
+    app.mainloop()
